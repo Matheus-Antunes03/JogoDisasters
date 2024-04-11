c1e93d41055d94e819920961f2415ef9232a98a2
Matheus-Antunes03 <matheus_pol@hotmail.com>
2024-04-10 21:18:58 -0300

    primeiro

---
+++ gameplay.js
@@ -0,0 +1,17 @@
+document.getElementById("botao").onclick = function () { 
+
+  let quantidade = document.getElementById("quantidade").value;
+
+//  fetch("http://localhost:3000/api/pedido", {
+//    method: "POST",
+//    body: JSON.stringify({
+//      idUsuario: "1",
+//      idProduto: "1",
+//      idEstabelecimento: "1",
+//      quantidade: quantidade,
+//    }),
+//    headers: {
+//      "Content-type": "application/json; charset=UTF-8",
+//    },
+//  });
+};
